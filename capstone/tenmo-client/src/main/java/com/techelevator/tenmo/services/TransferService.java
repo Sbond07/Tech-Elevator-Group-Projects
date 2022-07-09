698d48938e1e77aebf9a69100aff4b00cd95d5a8
Noni Crook <missnoni007@yahoo.com>
2022-07-09 14:22:24 -0400

    Updated AccountService, added TranferService, Transfer, Accunt classes

---
+++ capstone/tenmo-client/src/main/java/com/techelevator/tenmo/services/TransferService.java
@@ -0,0 +1,12 @@
+package com.techelevator.tenmo.services;
+
+import org.springframework.web.client.RestTemplate;
+
+public class TransferService {
+
+    private static final String API_BASE_URL = "http://localhost:8080/";
+    private final RestTemplate restTemplate = new RestTemplate();
+
+
+
+}
